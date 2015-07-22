3781a208230439c22383ec2a7127d293ad3aae1b
Mike Ferrara <mikef@broadcom.com>
2015-07-22 15:09:38 -0700

    Add freq, amplitude and output enable abstractions

--- Agilent_E8663D.pm
+++ Agilent_E8663D.pm
@@ -9,7 +9,6 @@ with( 'GPIBWrap', 'Throwable' );    #Use Try::Tiny to catch my errors
 # The following methods override those inherited from DC
 
 sub init {
-        # initialize for use as a dc resource
   my $self      = shift;
 
   return 0      if ( $self->{VIRTUAL} );
@@ -29,5 +28,50 @@ sub init {
   return 0;
 
 }
+
+#Set/Get CW frequency in Hz
+sub freq {
+  my $self=shift;
+  my $val=shift;
+
+  if (! defined($val)) {
+    return($self->iquery(":SOURCE:FREQUENCY:CW?"));
+  } else {
+    $self->iwrite(":SOURCE:FREQUENCY:CW $val;");
+    $self->iOPC();
+  }
+}
+
+#Set/Get Output Amplitude in dBm
+sub ampl {
+  my $self=shift;
+  my $val=shift;
+
+  if (! defined($val)) {
+    return($self->iquery(":POWER?"));
+  } else {
+    $self->iwrite(":POWER $val dBm;");
+    $self->iOPC();
+  }
+}
+
+sub outputState {
+  my $self=shift;
+  my $val=shift;
+
+  if (! defined($val)) {
+    return($self->iquery(":OUTPUT:STATE?"));
+  } else {
+    my $s=0;
+    SW: {
+        if ($val=~/on/i) { $s=1; last SW; }
+        if ($val=~/off/i) { $s=0; last SW; }
+        $s = ($val != 0);
+      }
+    $self->iwrite(":OUTPUT:STATE $s;");
+    $self->iOPC();
+  }
+}
+
 __PACKAGE__->meta->make_immutable;
 1;
