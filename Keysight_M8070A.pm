bf22601eb45a1a91e5905f572e4a4b33cf4d92d7
Mike Ferrara <mikef@broadcom.com>
2019-05-07 11:47:00 -0700

    Stub out M8070A

---
+++ Keysight_M8070A.pm
@@ -0,0 +1,30 @@
+# -*- mode: perl -*-
+package Keysight_M8070A;
+use Moose;
+use namespace::autoclean;
+
+with( 'GPIBWrap', 'Throwable' );    #Use Try::Tiny to catch my errors
+
+sub init {
+  my $self = shift;
+
+  return 0 if ( $self->{VIRTUAL} );
+
+  $self->iconnect();
+  $self->iwrite("*RST") if ( $self->{RESET} );    #Get us to default state
+
+  my $err = 'x';                                  # seed for first iteration
+                                                  # clear any accumulated errors
+  while ($err) {
+    $self->iwrite(":SYST:ERR?");
+    $err = $self->iread( 100, 1000 );
+    last if ( $err =~ /\+0/ );                    # error 0 means buffer is empty
+  }
+  $self->iwrite("*CLS");
+  #
+  return 0;
+
+}
+
+__PACKAGE__->meta->make_immutable;
+1;
